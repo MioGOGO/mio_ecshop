0eeb23d4e571a7aef8c049010904eab16f50f953
MioGOGO <miaoyanan0914@126.com>
2018-01-05 17:26:53 +0800

    ecshop

---
+++ ecshop/data/flashdata/dynfocus/data.js
@@ -0,0 +1,16 @@
+imgUrl1="data/afficheimg/20161114nraqzu.jpg";
+imgtext1="";
+imgLink1=escape("https://car2.autoimg.cn/cardfs/product/g21/M02/A7/E5/1024x0_1_q87_autohomecar__wKgFWlhSdMCAYVYYAAiS2Me17ms894.jpg");
+imgUrl2="data/afficheimg/20161114prfywc.jpg";
+imgtext2="";
+imgLink2=escape("https://car2.autoimg.cn/cardfs/product/g21/M02/A7/E5/1024x0_1_q87_autohomecar__wKgFWlhSdMCAYVYYAAiS2Me17ms894.jpg");
+imgUrl3="data/afficheimg/20161114uxmgov.png";
+imgtext3="";
+imgLink3=escape("https://car2.autoimg.cn/cardfs/product/g21/M02/A7/E5/1024x0_1_q87_autohomecar__wKgFWlhSdMCAYVYYAAiS2Me17ms894.jpg");
+imgUrl4="data/afficheimg/20161114ypdlyr.jpg";
+imgtext4="";
+imgLink4=escape("https://car2.autoimg.cn/cardfs/product/g21/M02/A7/E5/1024x0_1_q87_autohomecar__wKgFWlhSdMCAYVYYAAiS2Me17ms894.jpg");
+
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3+"|"+imgUrl4;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3+"|"+imgLink4;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3+"|"+imgtext4;
