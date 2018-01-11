c3a7095d1677dc02fdb95503b920865148bfc169
MioGOGO <miaoyanan0914@126.com>
2018-01-11 16:24:39 +0800

    bugfix

--- appserver/app/Models/dckc/Goods.php
+++ appserver/app/Models/dckc/Goods.php
@@ -12,6 +12,36 @@ use App\Models\BaseModel;
 
 class Goods extends BaseModel{
 
+    protected $connection = 'shop';
+
+    protected $table      = 'goods';
+
+    public    $timestamps = false;
+
+    protected $primaryKey = 'goods_id';
+
+    protected $guarded = [];
+
+    protected $appends = [
+        'id', 'category', 'brand', 'shop', 'sku', 'default_photo', 'photos', 'name', 'price', 'current_price', 'discount', 'sales_count','score','good_stock',
+        'comment_count', 'is_liked', 'review_rate', 'intro_url', 'share_url', 'created_at', 'updated_at'
+    ];
+
+    protected $visible = [
+        'id', 'category', 'brand', 'shop', 'tags', 'default_photo', 'photos','sku', 'name', 'price', 'current_price', 'discount', 'is_shipping', 'promos','stock','properties','sales_count', 'attachments','goods_desc','score','comments','good_stock','comment_count', 'is_liked', 'review_rate', 'intro_url', 'share_url', 'created_at', 'updated_at'
+    ];
+
+    // protected $with = [];
+
+    const NOSORT     = 0;
+    const PRICE      = 1;
+    const POPULAR    = 2;
+    const CREDIT     = 3;
+    const SALE       = 4;
+    const DATE       = 5;
+
+    const ASC        = 1;
+    const DESC       = 2;
 
 
     /**
