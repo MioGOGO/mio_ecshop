68a70355dc644fd14730a6c44310ab738e270728
MioGOGO <miaoyanan0914@126.com>
2018-01-25 17:51:50 +0800

    bugfix

--- appserver/app/Http/Controllers/dckc/OrderController.php
+++ appserver/app/Http/Controllers/dckc/OrderController.php
@@ -9,9 +9,11 @@
 namespace App\Http\Controllers\dckc;
 
 
-use App\Helper\Token;
 use App\Http\Controllers\Controller;
 use App\Models\dckc\Member;
+use Validator;
+
+
 
 class OrderController extends Controller
 {
@@ -29,12 +31,42 @@ class OrderController extends Controller
 
         $userinfo = Member::authDckc( $this->validated );
 
-        $decodeJson = Token::jsonDecode( $this->validated['params'] );
+        $rulesJson = [
+            'address'   => 'required|string|min:1',
+            'dishTime'  => 'required|string|min:1',
+            'goodsList' => 'required|string|min:1',
+            'totalFee'  => 'required|string|min:1',
+            'note'      => 'required|string|min:1',
+            'payType'   => 'required|string|min:1',
+
+        ];
+        if( $error = $this->vaidJsonOrderParsmrs( json_decode( $this->validated['params'],true ),$rulesJson ) ){
+            return $error;
+        }
 
 
 
-        print_r( $decodeJson );
 
+        print_r( $error );
+
+    }
+
+
+    /**
+     * 验证json数据
+     * @param  array $rules
+     * @return response
+     */
+    public function vaidJsonOrderParsmrs($decodeJson,$rules)
+    {
+        $validator = Validator::make($decodeJson, $rules);
+        if ($validator->fails()) {
+            return self::jsondckc(BaseModel::formatErrorDckc(BaseModel::BAD_REQUEST, $validator->messages()->first()));
+        } else {
+            $res = array_intersect_key($decodeJson, $rules);
+            $res = $decodeJson ;
+            return false;
+        }
     }
 
 
