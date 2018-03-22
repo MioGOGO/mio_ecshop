af083753cb2b69deb7e25e32c9a941f2161d9c77
miaoyanan <miaoyanan@baidu-mgame.com>
2018-03-22 11:18:25 +0800

    qr code

--- appserver/app/Http/routes.php
+++ appserver/app/Http/routes.php
@@ -40,6 +40,7 @@ $app->group(['namespace' => 'App\Http\Controllers\dckc','prefix' => 'dckc', 'mid
     $app->get('delivery/checkRange','DeliveryController@checkRange');
     $app->get('user/login','UserController@authDckc');
     $app->get('seller/login','UserController@sellerAuth');
+    $app->get('qr/get','OrderController@createQr');
 
 });
 
