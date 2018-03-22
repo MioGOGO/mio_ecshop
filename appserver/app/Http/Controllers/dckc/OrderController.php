0b0370e1248e2bc1623e0eb39af48e53d51d18ba
miaoyanan <miaoyanan@baidu-mgame.com>
2018-03-22 15:13:49 +0800

    qr code

--- appserver/app/Http/Controllers/dckc/OrderController.php
+++ appserver/app/Http/Controllers/dckc/OrderController.php
@@ -124,11 +124,11 @@ class OrderController extends Controller
             return $error;
         }
         $info = $this->validated['id'];
-        $info = serialize( $info );
-        if(!file_exists(base_path('public/qrcodes/'.$info.'.png'))){
-            QrCode::format('png')->size(200)->merge('/public/img/logo.gif',.15)->generate($info,base_path('public/qrcodes/'.$info.'.png'));
+        $info = urlencode( $info );
+        if(!file_exists(base_path('public/img/qrcodes/'.$info.'.png'))){
+            QrCode::format('png')->size(200)->merge('/public/img/logo.gif',.15)->generate($info,base_path('public/img/qrcodes/'.$info.'.png'));
         }
-        $img = base_path('public/qrcodes/'.$this->validated['id'].'png');
+        $img = 'img/qrcodes/'.$info.'png';
         //$a = QrCode::generate('Hello,LaravelAcademy!');
         //echo "<img src='$a>";
 
