e2bb5d494c2d033b864450e5138b5f8ed64cf50a
miaoyanan <miaoyanan@baidu-mgame.com>
2018-03-23 14:59:11 +0800

    bugfix

--- appserver/app/Models/dckc/Order.php
+++ appserver/app/Models/dckc/Order.php
@@ -197,8 +197,10 @@ class Order extends BaseModel {
     public static function getDetailSeller(array $attributes)
     {
         extract($attributes);
-        Token::authorizationSeller();
-
+        $uid = Token::authorizationSeller();
+        if( !$uid ){
+            return self::formatErrorDckc( 8003,'this order seller Permission denied' );
+        }
         $model = self::where('order_sn',$id);
         $model->whereIn('pay_status', [self::PS_PAYED]);
         $model->whereIn('order_status', [self::OS_UNCONFIRMED]);
